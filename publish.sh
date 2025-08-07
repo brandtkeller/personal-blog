454d28f85d1b67d7bae7002b61caf522ecc06cb9
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-08-07 03:20:53 +0000

    fix(publish): update publish target
    Signed-off-by: Brandt Keller <brandt.keller@defenseunicorns.com>

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ echo "Publishing Branch: $CF_PAGES_BRANCH"
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   # Set baseurl to the production URL
   echo "Running the production publish"
-  hugo -b https://missionfocused.dev
+  hugo -b https://brandtkeller.com
 else
   # Else set it to the expected dev url
   echo "Running the development preview publish"
